d95d40d0400e7474f3eb6d9a52d677c9759de732
Derick Quartey <niikwartey1@gmail.com>
2016-10-26 13:55:13 -0400

    Done.

--- components/Pet.js
+++ components/Pet.js
@@ -3,24 +3,31 @@ const React = require('react');
 class Pet extends React.Component {
   constructor() {
     super();
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler() {
+    this.props.onAdoptPet(this.props.pet.id);
   }
 
   render() {
+    const gender = this.props.pet.gender[0];
+    var genderIcons = {'m': '♂', 'f': '♀'};
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">{this.props.pet.name} ({genderIcons[gender]})</a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">Pet type: {this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
+          { this.props.isAdopted ? <button className="ui disabled button">Already adopted</button> :
+          <button className="ui primary button" onClick={this.clickHandler}>Adopt pet</button> }
         </div>
       </div>
     );
