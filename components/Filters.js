bc05651f2068817e576a16d0a9e3161503b4c541
Natalisp <nathalipat@gmail.com>
2016-12-12 22:57:05 -0500

    Done.

--- components/Filters.js
+++ components/Filters.js
@@ -1,8 +1,14 @@
 const React = require('react');
 
 class Filters extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
+
+    this.handleFilter = this.handleFilter.bind(this)
+  }
+
+  handleFilter(e) {
+    this.props.onChangeType(e.target.value)
   }
 
   render() {
@@ -10,7 +16,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" value={this.props.filters.type} onChange={this.handleFilter}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,7 +25,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -3,13 +3,34 @@ const React = require('react');
 const Pet = require('./Pet');
 
 class PetBrowser extends React.Component {
-  render() {
-    return (
-      <div className="ui cards">
-        <code>&lt;Pet /&gt;</code> &nbsp; components should go here
-      </div>
-    );
+  constructor(props) {
+    super()
+
+  this.checkAdoptionStatus = this.checkAdoptionStatus.bind(this)
+}
+
+checkAdoptionStatus(pet) {
+  if (this.props.adoptedPets.includes(pet.id)) {
+    return true
+  } else {
+    return false
   }
 }
+
+
+  render() {
+    const pets = this.props.pets.map((pet, index) => {
+      return(
+      <Pet key={index} pet = {pet} isAdopted = {this.checkAdoptionStatus(pet)} onAdoptPet = {this.props.onAdoptPet} />
+    )
+  })
+
+        return (
+          <div className="ui cards">
+            {pets}
+          </div>
+        );
+      }
+    }
 
 module.exports = PetBrowser;
